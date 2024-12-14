28ca87c28f1061ba13ff6b3608ba08248e352784
emefutty <ekrasnoslobodtseva@bk.ru>
2024-12-14 22:47:39 +0300

    Update hello_world.cpp

--- hello_world.cpp
+++ hello_world.cpp
@@ -2,7 +2,7 @@
 #include <iostream>
 int main() {
 	std::string name;
-	std::cout << "Введите имя";
+	std::cout << "Введите имя"; #Введите имя с большой буквы
 	std::cin>>name;
 	std::cout<< "Hello world from"<< name << std::endl;
 	return 0;
